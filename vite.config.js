238540e81844aec3d66b4fe473368f755dd96086
e10gb <egib557@aucklanduni.ac.nz>
2025-05-16 19:45:52 +1200

    Changed vite.config folder

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: "/e10gb"
-})
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
+});

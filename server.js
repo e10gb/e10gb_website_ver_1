a09f5edeb21874acfdd27129d8f40bca4bcf4278
e10gb <egib557@aucklanduni.ac.nz>
2025-05-21 15:48:37 +1200

    test

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.static(path.join(__dirname, 'dist')));
 
 // Catch-all to serve index.html for SPA routes
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(port, () => {
